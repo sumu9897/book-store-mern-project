1a1e69036ef52f6e54a87b734ed96c7c18ee745d
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-10-21 21:53:34 +0600

    Login Page design as per figma file

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Login = () => {
   return (
-    <div>
-      Login Page
+    <div className='h-[calc(100vh-120px)] flex justify-center items-center'>
+        <div className='w-full max-w-sm mx-auto shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+            <h2 className='text-xl font-semibold mb-4'>Please Login</h2>
+            <form>
+                <div className='mb-4'>
+                    <label className='block text-gray-700 text-sm font-bold mb-2' 
+                    htmlFor="email">Email</label>
+                    <input type="email" name='email' id='email' placeholder='Email Address' 
+                    className='shadow appearance-none border rounded w-full py-2 px-3 
+                    leading-tight focus:outline-none focus:shadow'
+                    />
+                </div>
+                <div className='mb-4'>
+                    <label className='block text-gray-700 text-sm font-bold mb-2' 
+                    htmlFor="password">Password</label>
+                    <input type="password" name='password' id='password' placeholder='Password' 
+                    className='shadow appearance-none border rounded w-full py-2 px-3 
+                    leading-tight focus:outline-none focus:shadow'
+                    />
+                </div>
+                <div className=''>
+                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none'>Login</button>
+                </div>
+            </form>
+            <p className='align-baseline font-medium mt-4 text-sm'>Haven't an account? Please <Link to="/register" className='text-blue-500 hover:text-blue-700'>Register</Link></p>
+        </div>
     </div>
   )
 }
